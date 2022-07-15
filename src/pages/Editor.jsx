686cfd8369dd8bd64cd0ebb8413fcf61d8af637a
Manuel Alvarez <alvarezmanuelignacio@gmail.com>
2022-07-15 16:44:27 -0300

    add rich text editor app

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { HtmlEditor, Image, Inject, Link, QuickToolbar, RichTextEditorComponent, Toolbar } from '@syncfusion/ej2-react-richtexteditor';
+
+import { EditorData } from '../data/dummy';
+import { Header } from '../components';
+
 
 const Editor = () => {
   return (
-    <div>
-      Editor
+    <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
+      {/* Header */}
+      <Header category="App" title='Editor' />
+      {/* Rich Editor */}
+      <RichTextEditorComponent>
+        <EditorData />
+        <Inject services={[Toolbar, Link, Image, HtmlEditor, QuickToolbar]} />
+      </RichTextEditorComponent>
     </div>
   )
 }
