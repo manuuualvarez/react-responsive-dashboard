77b7f3d1f120801615e67c96d754a28c28b71155
Manuel Alvarez <alvarezmanuelignacio@gmail.com>
2022-07-15 15:52:25 -0300

    add employees page with table

--- src/pages/Employee.jsx
+++ src/pages/Employee.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Search, Toolbar, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
+import { employeesData, employeesGrid } from '../data/dummy';
+import { Header } from '../components';
+
 
 const Employee = () => {
   return (
-    <div>
-      Employee
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      {/* Header */}
+      <Header title="Employees" category="Page"/>
+      {/* Table */}
+      <GridComponent
+        dataSource={employeesData}
+        allowPaging={true}
+        allowSorting={true}
+        toolbar={['Search']}
+        width="auto"
+      >
+        <ColumnsDirective>
+        {employeesGrid.map((item, index) => <ColumnDirective key={index} {...item}/>)}
+        </ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar]}/>
+      </GridComponent>
     </div>
   )
 }
